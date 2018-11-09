016303405ca0602a1199a9c632e79da355a73d08
unknown <928231861@qq.com>
2018-11-09 13:03:11 +0800

    小程序练习

---
+++ pages/logs/logs.js
@@ -0,0 +1,84 @@
+var newListData = require('../../data/new-list.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    dataList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      dataList: newListData.newList
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  /**
+   * 点击列表进入详情页
+   */
+  pushNewInfo(event) {
+    let newId = event.currentTarget.dataset.newid;
+    wx.navigateTo({
+      url: '../newinfo/newinfo',
+    });
+  },
+
+  /**
+   * 点赞
+   */
+  addXing(event) {
+  }
+})
